b4bb1108590655f990262a2b7fbebcc887d8022c
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-25 10:36:26 -0400

    artist model working

--- music/models.py
+++ music/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Album(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.CharField(max_length=200)
+    artist = models.ForeignKey(to="Artist", on_delete=models.CASCADE)
     year = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
@@ -27,7 +27,6 @@ class Label(models.Model):
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
-    # album = models.ForeignKey(to="Album"), on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
