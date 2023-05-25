60b12caf3baab42e161d1301a7d9375fa77e3515
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-24 21:00:58 -0400

    added artist and label models

--- music/models.py
+++ music/models.py
@@ -9,9 +9,25 @@ class Album(models.Model):
     artist = models.CharField(max_length=200)
     year = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
-
     is_active = models.BooleanField(default=True)
+    label = models.ForeignKey(
+        to="Label", on_delete=models.CASCADE, blank=True, null=True)
 
 
 def __str__(self):
     return self.title
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    # album = models.ForeignKey(to="Album"), on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
